4509e23b1ae35d7c50177f0f18fb4c117352cf69
KurtBliss <kurtbliss3@gmail.com>
2020-02-09 20:54:49 -0500

    updated channel.php

--- views/youtube/channel.php
+++ views/youtube/channel.php
@@ -2,11 +2,15 @@
 
 $title = "Channel - Abutube";
 
+global $content;
+
+// http://localhost:8080/channel/UCr3cBLTYmIK9kY0F_OdFWFQ
+
 $content = <<<HTML
     <main>
         <section>
             <!-- Channel Information -->
-            <p></p>
+            <p>Channel $id</p>
         </section>
         <section>
 
