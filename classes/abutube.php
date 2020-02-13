b958bb978bb16b5707c035632703e38272414e9a
KurtBliss <kurtbliss3@gmail.com>
2020-02-13 13:30:05 -0500

    updated abutube.php

--- classes/abutube.php
+++ classes/abutube.php
@@ -1,36 +1,22 @@
 <?php
-
 require_once "vendor/autoload.php";
-
-use GuzzleHttp\Client;
-
 
 class abutube {
     private $secretJson, $secretKey;
-
-    public $client;
-
     function __construct() {
-        // Secrets from github
         $this->secretJson =  json_decode(file_get_contents("secret.json"));
         $this->secretKey = $this->secretJson->devKey;
-
-        // Init for making http request
-        $this->client = new Client([
-            "base_uri" => "https://www.googleapis.com/youtube/v3",
-            "timeout"  => 2.0
-        ]);
     } 
 
-    function search() {
-        
-        echo $this->client->request("GET", "search", [
-                "form_params" => [
-                "key" => $this->secretKey,
-                "q" => "pokemon",
-                "part" => "snippet",
-                "maxResults" => 25,]
-        ]);
+    function search($q, $maxResults = 25) {
+        return json_decode(file_get_contents("https://www.googleapis.com/youtube/v3/search?key=" . $this->secretKey . "&q=$q&part=snippet&maxResults=$maxResults"));
+    }
+
+    function get($url, $params) {
+        $varUrl = $url . "?";
+        foreach($params as $param) {
+            $varUrl .= $param . "&";
+        }
     }
 }
 
