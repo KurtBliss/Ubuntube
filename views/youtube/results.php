8c8b777014a3f3b9e760954716ae1beeae3e4e7b
KurtBliss <kurtbliss3@gmail.com>
2020-02-13 13:30:19 -0500

    updated results.php

--- views/youtube/results.php
+++ views/youtube/results.php
@@ -1,56 +1,38 @@
 <?php
 
-$client = new Google_Client();
-$client->setDeveloperKey($devKey);
-
-$youtube = new Google_Service_YouTube($client);
-
-$searchResponse = $youtube->search->listSearch('id,snippet', [
-    'q' => $_GET['q'],
-    'maxResults' => $_GET['maxResults'],
-]);
-
-$videos = '';
-$channels = '';
-$playlists = '';
-
-foreach ($searchResponse['items'] as $searchResult) {
-    switch ($searchResult['id']['kind']) {
-        case 'youtube#video':
-            $videos .= sprintf(
-                '<li>%s (%s)</li>',
-                $searchResult['snippet']['title'],
-                $searchResult['id']['videoId']
-            );
-            break;
-        case 'youtube#channel':
-            $channels .= sprintf(
-                '<li>%s (%s)</li>',
-                $searchResult['snippet']['title'],
-                $searchResult['id']['channelId']
-            );
-            break;
-        case 'youtube#playlist':
-            $playlists .= sprintf(
-                '<li>%s (%s)</li>',
-                $searchResult['snippet']['title'],
-                $searchResult['id']['playlistId']
-            );
-            break;
-    }
-}
-
-// make search into function 
-// make item function... 
-
-$q = "";
-
+global $title;
 global $content;
+
+$q = $_GET["q"];
+
+require_once "classes/abutube.php";
+
+$abutube = new abutube;
+
+$response = $abutube->search($q);
+
+$results = "";
+$snippet;
+$resultTitle = "";
+
+foreach($response->items as $item) {
+    if (isset($item->snippet))
+        $resultTitle = $item->snippet->title;
+    else
+        $resultTitle = "";
+
+    $results .= <<<HTML
+        <div>
+            <p>$resultTitle</p>
+        </div>
+    HTML;
+}
 
 $content = <<<HTML
     <main>
         <section>
             <p>Showing results for $q</p>
+            $results
         </section>
     </main>
 HTML;
