20c8ce0c2dbd8e574bb3bf04fb24d8bd1ed51249
KurtBliss <kurtbliss3@gmail.com>
2020-02-18 17:04:30 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,17 @@
 <?php
 
 echo "running test.php\n";
-// require_once "classes/abutube.php";
-// $abutube = new abutube;
 
-// echo "$abutube->search();
+require_once "abutube.php";
 
-// $abutube->hi();
+$response = abutube::channel_data($_GET["id"]);
 
+print_r(abutube::playlist_items(
+    $response->items[0]->contentDetails->relatedPlaylists->uploads
+));
+
+/*
+
+Channel Title, Thumbnail
+
+*/
