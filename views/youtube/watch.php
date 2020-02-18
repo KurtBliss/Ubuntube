4e2cedd2fc4835a30f648d039480cbe55c1e7635
KurtBliss <kurtbliss3@gmail.com>
2020-02-18 17:04:45 -0500

    Update watch.php

--- views/youtube/watch.php
+++ views/youtube/watch.php
@@ -2,12 +2,34 @@
 
 $videoId = $_GET["v"];
 
+$response = abutube::video_data($videoId);
+
+$videoTitle = $response->items[0]->snippet->title;
+$videoDesc = $response->items[0]->snippet->description;
+$channelTitle = $response->items[0]->snippet->channelTitle;
+$channelId = $response->items[0]->snippet->channelId;
+
+$response = abutube::channel_data($channelId);
+$channelThumbnails = $response->items[0]->snippet->thumbnails;
+$channelThumbnail = $channelThumbnails->default->url;
+
 global $content;
 
 $content = <<<HTML
     <main>
         <section>
+            <p class="section-title">$videoTitle</p>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/$videoId" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div>
+                
+                <p class="watch-channel-title">
+                    <a href="/channel/$channelId">$channelTitle</a>
+                </p>
+                
+                <img class="watch-channel-thumbnail" src="$channelThumbnail">
+                
+                <p>$videoDesc</p>
+            </div>
         </section>
     </main>
 HTML;
