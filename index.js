35de32a2044205aaddc28ec63f49667c9a0bc0a8
KurtBliss <kurtbliss3@gmail.com>
2020-02-19 00:14:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello");
+function search(event) {
+  if (event.key === "Enter") {
+    window.location.href =
+      "/results?q=" + document.getElementById("searchInput").value;
+  } else {
+    return false;
+  }
+}
