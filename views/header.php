1e26c0ac0ae5ba8031d1d74ad8d89ce06c7b21e8
KurtBliss <kurtbliss3@gmail.com>
2020-02-19 00:15:04 -0500

    Update header.php

--- views/header.php
+++ views/header.php
@@ -3,7 +3,18 @@
 global $header;
 
 $header = <<<HTML
-    <header>
-        <p>blissfultube</p>
-    </header>
+<header>
+  <!--   Logo   -->
+  <label>
+    <p class="header-title">
+      BLISSFULTUBE
+    </p>
+  </label>
+  
+  <!--  Navigation  -->
+  <nav class="header-nav">
+    <a>home</a> | <a>about</a> | <a>contact</a> | <input id="searchInput" type="text" placeholder="Search.." onkeypress="search(event)">
+  </nav>
+  <hr> 
+</header>
 HTML;
