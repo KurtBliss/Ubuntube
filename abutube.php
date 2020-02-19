7cd320d1b083a12dfdd9a258a2701b90d3447260
KurtBliss <kurtbliss3@gmail.com>
2020-02-19 00:14:18 -0500

    Update abutube.php

--- abutube.php
+++ abutube.php
@@ -3,32 +3,35 @@ require_once "vendor/autoload.php";
 
 $_ENV["YOUTUBE_DEV_KEY"] = json_decode(file_get_contents("secret.json"))->devKey;
 
-class abutube {
-    private $secretJson, $secretKey;
-
-    function search($q, $maxResults = 25) {
+class abutube
+{
+    function search($q, $maxResults = 25)
+    {
         return abutube::youtube("search", [
-            "q" => $q, 
-            "part" => "snippet", 
+            "q" => $q,
+            "part" => "snippet",
             "maxResults" => $maxResults
         ]);
     }
 
-    function video_data($id) {
+    function video_data($id)
+    {
         return abutube::youtube("videos", [
             "id" => $id,
             "part" => "snippet,contentDetails,statistics"
         ]);
     }
 
-    function playlist_data($id) {
+    function playlist_data($id)
+    {
         return abutube::youtube("playlists", [
             "id" => $id,
             "part" => "snippet,contentDetails"
         ]);
     }
 
-    function playlist_items($id, $max = 25) {
+    function playlist_items($id, $max = 25)
+    {
         return abutube::youtube("playlistItems", [
             "playlistId" => $id,
             "part" => "snippet,contentDetails",
@@ -36,14 +39,16 @@ class abutube {
         ]);
     }
 
-    function channel_data($id) {
+    function channel_data($id)
+    {
         return abutube::youtube("channels", [
             "id" => $id,
             "part" => "snippet,contentDetails"
         ]);
     }
 
-    function channel_sections($id) {
+    function channel_sections($id)
+    {
         return abutube::youtube("channelSections", [
             "channelId" => $id,
             "part" => "snippet,contentDetails,targeting",
@@ -51,15 +56,22 @@ class abutube {
         ]);
     }
 
-    function youtube($resource, $params) {
+    function youtube($resource, $params)
+    {
         $parse = "";
-        foreach($params as $key => $param) {
+        foreach ($params as $key => $param) {
             if (isset($param)) {
                 $parse .= "&" . $key . "=" . $param;
             }
         }
+
+        $encode = "https://www.googleapis.com/youtube/v3/" . $resource . "?key=" .  $_ENV["YOUTUBE_DEV_KEY"] . $parse;
+
+        htmlentities($encode);
+        urlencode($encode);
+
         return json_decode(
-            file_get_contents("https://www.googleapis.com/youtube/v3/" . $resource . "?key=" .  $_ENV["YOUTUBE_DEV_KEY"] . $parse
-        ));
+            file_get_contents($encode)
+        );
     }
 }
