36e1dd5e1f2dc31cc61f027bdee2725ac24c6638
KurtBliss <kurtbliss3@gmail.com>
2020-03-01 00:24:01 -0500

    Update edit.php

--- views/feed/edit.php
+++ views/feed/edit.php
@@ -1,49 +1,21 @@
 <?php
 $feedId = $id;
-
 global $content;
 
 $script = <<<JS
-    console.log(loadObject(document.getElementById("feedNameInput").value))
+    var feedsObj = feeds();
 
-    if (loadObject(document.getElementById("feedNameInput").value) === null) {
-        var feed = {
-            name: "$feedId",
-            id: "$feedId"
-        };
-    } else {
-        var feed = loadObject(document.getElementById("feedNameInput").value);
-        getElementById("feedNameInput").value = feed.name;
-    }
-
-    function onFeedName(event) {
-        if (event.key === "Enter") {
-            new_feed_name = document.getElementById("feedNameInput").value;
-            console.log("new_feed_name", new_feed_name);
-            feed.name = document.getElementById("feedNameInput").value;
-            saveObject(feed.name, feed);
-        } else {
-            return false;
-        }
-    }
-
-    function saveObject(name, object) {
-        localStorage.setItem("obj_" + name, JSON.stringify(object));
-    }
-
-    function loadObject(name) {
-        if (localStorage.getItem("obj_" + name) === null) 
-            return null;
-        return JSON.parse(localStorage.getItem("obj_" + name));
-    }
+    if ("$feedId" in feedsObj) {
+        document.getElementById("feedNameInput").value = feedsObj["$feedId"].name;
+    } 
 JS;
 
 $content = <<<HTML
-    <script>$script</script>
     <main>
         <section>
             <label>feed name</label>
             <input type="text" id="feedNameInput" onkeypress="onFeedName(event)" value="$feedId">
         </section>
     </main>
+    <script>$script</script>
 HTML;
