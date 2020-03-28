20749d0d2853c0036502bb7ef62f3059a20a27e7
KurtBliss <kurtbliss3@gmail.com>
2020-03-28 19:43:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -140,3 +140,20 @@ function saveObject(name, object) {
 function loadObject(name) {
   return JSON.parse(localStorage.getItem("obj_" + name));
 }
+
+
+/*
+
+    Avoid links exiting ios fullscreen mode
+
+*/
+
+var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++) {
+    if(!a[i].onclick && a[i].getAttribute("target") != "_blank") {
+        a[i].onclick=function() {
+                window.location=this.getAttribute("href");
+                return false; 
+        }
+    }
+}
