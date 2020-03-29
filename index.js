92c65b6a78c74db5e153afc714f1a0511649a1d7
KurtBliss <kurtbliss3@gmail.com>
2020-03-29 02:13:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -161,8 +161,9 @@ function loadObject(name) {
 var a=document.getElementsByTagName("a");
 for(var i=0;i<a.length;i++)
 {
-    a[i].onclick=function()
+    a[i].onclick=function(e)
     {
+      e.preventDefault();
         window.location=this.getAttribute("href");
         return false
     }
