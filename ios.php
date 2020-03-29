d6b74902bcd664988ec29081a2f538e2d074a57f
KurtBliss <kurtbliss3@gmail.com>
2020-03-29 01:49:59 -0400

    Update ios.php

--- ios.php
+++ ios.php
@@ -7,7 +7,17 @@
 */
 
 echo <<<HTML
-<iframe src="/" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
-    Your browser doesn't support iframes
-</iframe>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport"; content="width=device-width, initial-scale=1.0, user-scalable=no"; />
+        <meta name="apple-mobile-web-app-capable" content="yes">
+    </head>
+    <body>
+        <iframe src="/" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+            Your browser doesn't support iframes
+        </iframe>
+    </body>
+</html>
 HTML;
+
