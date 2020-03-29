5f2b028d8a0203c5e1e68f3873675a1b7c7699f1
KurtBliss <kurtbliss3@gmail.com>
2020-03-29 02:00:01 -0400

    Update ios.php

--- ios.php
+++ ios.php
@@ -12,6 +12,8 @@ echo <<<HTML
         <meta charset="utf-8">
         <meta name="viewport"; content="width=device-width, initial-scale=1.0, user-scalable=no"; />
         <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="viewport" content="width=device-width,initial-scale=1.01,minimal-ui">
+
     </head>
     <body>
         <iframe src="/" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
