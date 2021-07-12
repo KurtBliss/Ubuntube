a75550136e902753cb4ea1163a1c330cfd890565
KurtBliss <kurtbliss3@gmail.com>
2021-07-12 08:23:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ session_start();
 if (isset($_GET["access_token"])) {
     echo "has access_token!";
     $_SESSION["access_token"] = $_GET["access_token"];
+} else {
+    echo "no token :(";
 }
 
 echo <<<HTML
