b41df58cabe5d0acfac67f8b3993947cba3a4949
KurtBliss <kurtbliss3@gmail.com>
2021-07-13 23:13:49 -0400

    Update auth.php

--- auth.php
+++ auth.php
@@ -5,8 +5,8 @@ require_once 'vendor/google/apiclient-services/src/YouTube.php';
 $client = new Google_Client();
 $client->setAuthConfig('client_secret.json');
 $client->addScope(GOOGLE_SERVICE_YOUTUBE::YOUTUBE_FORCE_SSL);
-// $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php');
-$client->setRedirectUri('http://127.0.0.1:8080/auth_process.php');
+$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/auth_process.php');
+// $client->setRedirectUri('http://127.0.0.1:8080/auth_process.php');
 // offline access will give you both an access and refresh token so that
 // your app can refresh the access token without user interaction.
 $client->setAccessType('offline');
