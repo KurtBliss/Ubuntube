53f2c56f2f320164c99019678c185b189148f9b9
KurtBliss <kurtbliss3@gmail.com>
2021-08-04 08:37:18 -0400

    Update menu.php

--- views/menu.php
+++ views/menu.php
@@ -2,14 +2,29 @@
 
 global $menu;
 
+$script = <<<JS
+    homeLoadFeeds("feeds-list-2");
+JS;
+
 $menu = <<<HTML
 
   <aside id="asideMenu">
     <ul>
       <li><a href="/auth">login</a></li>
       <li><a href="/subscriptions">subscriptions</a></li>
-      <li></li>
     </ul>
+
+    <h1 class="feed-home-section">Your Feeds:</h1>
+    <section>
+        <div id="feeds-list-2"></div>
+    </section>
+
+    <h1 class="feed-home-section">Make New Feed:</h1>
+    <section>
+        <input type="text" id="newFeedInput" onkeypress="onFeedNew(event)"><button onclick="NewFeedButton()">Create</button>
+    </section>
     
+    <script>$script</script>
+
   </aside>
 HTML;
